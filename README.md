7370e21b8bc74c7fb0c85b1a5f616616283f5b47
Nabarun Pal <pal.nabarun95@gmail.com>
2020-07-01 14:51:38 +0530

    Add examples
    Signed-off-by: Nabarun Pal <pal.nabarun95@gmail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# Airflow Experiments
+
+Just a repository to play around with Airflow.
+
+Some DAGs are sourced from the Airflow examples.
---
+++ dags/example_branch_operator.py
@@ -0,0 +1,53 @@
+"""Example DAG demonstrating the usage of the BranchPythonOperator."""
+
+import random
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import BranchPythonOperator
+from airflow.utils.dates import days_ago
+
+args = {
+    'owner': 'airflow',
+    'start_date': days_ago(2),
+}
+
+dag = DAG(
+    dag_id='example_branch_operator',
+    default_args=args,
+    schedule_interval="@daily",
+    tags=['example']
+)
+
+run_this_first = DummyOperator(
+    task_id='run_this_first',
+    dag=dag,
+)
+
+options = ['branch_a', 'branch_b', 'branch_c', 'branch_d']
+
+branching = BranchPythonOperator(
+    task_id='branching',
+    python_callable=lambda: random.choice(options),
+    dag=dag,
+)
+run_this_first >> branching
+
+join = DummyOperator(
+    task_id='join',
+    trigger_rule='none_failed_or_skipped',
+    dag=dag,
+)
+
+for option in options:
+    t = DummyOperator(
+        task_id=option,
+        dag=dag,
+    )
+
+    dummy_follow = DummyOperator(
+        task_id='follow_' + option,
+        dag=dag,
+    )
+
+    branching >> t >> dummy_follow >> join
