5bf9b4a622a460499e587145291714aa2e11d0fd
Nabarun Pal <pal.nabarun95@gmail.com>
2020-07-01 16:29:58 +0530

    Modify bash operator schedule
    Signed-off-by: Nabarun Pal <pal.nabarun95@gmail.com>

--- dags/example_bash_operator.py
+++ dags/example_bash_operator.py
@@ -15,7 +15,7 @@ args = {
 dag = DAG(
     dag_id='example_bash_operator',
     default_args=args,
-    schedule_interval='0 0 * * *',
+    schedule_interval='*/10 * * * *',
     dagrun_timeout=timedelta(minutes=60),
     tags=['example']
 )
